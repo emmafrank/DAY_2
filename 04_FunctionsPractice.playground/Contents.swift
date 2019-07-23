62f8e19227be19c71be709c69d6956e995bf004d
Apple <apple@Apples-MacBook-Pro-30.local>
2019-07-23 07:44:56 -0700

    first commit

---
+++ 04_FunctionsPractice.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+func robotBaker(floursugar : Int) -> Int {
+    let numberOfCakes = floursugar * 3
+    
+     print ("Hi! I'm going to make you \(numberOfCakes) cakes")
+return numberOfCakes
+}
+
+let ingredients = robotBaker(floursugar: 5)
+
+
+//print ("Open the door")
+//print ("Go outside")
+//print ("Do this \(numberOfDogs) times")
+//let lengthOfWalk = numberOfDogs * 15
+//print ("\(lengthOfWalk) minutes" )
+//return lengthOfWalk
